7d0d61e0d311971d2b664b70c6c6221c7a378d88
Kevin Hwang <k3vinhwang@gmail.com>
2013-04-18 19:43:56 -0700

    Initial commit--RefineryCMS locked and loaded!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dancersden::Application.config.secret_token = '47f58bdb8b6181c0c434a1c31d9104a2b0bb56ac05cf654261f362a8c5d07c504c3f887e6d37f5767405c90225a0e0b4271281018f35e37b4f604dc7417efb92'
